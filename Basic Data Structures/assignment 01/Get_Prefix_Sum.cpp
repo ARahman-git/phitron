47d6b2bbdf1ff22e1396dca3982e0723d2609ced
ARahman-git <139566806+ARahman-git@users.noreply.github.com>
2026-06-27 21:12:17 +0600

    prefix sum

--- Basic Data Structures/assignment 01/Get_Prefix_Sum.cpp
+++ Basic Data Structures/assignment 01/Get_Prefix_Sum.cpp
@@ -5,6 +5,21 @@ int main()
     int n;
     cin >> n;
     vector <long long> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    vector<long long> v2(n);
+    v2[0] = v[0];
+    for (int i = 1; i < n; i++)
+    {
+        v2[i] = v2[i-1] + v[i];
+    }
+
+    for (int i = n-1; i >= 0; i--)
+    {
+        cout << v2[i] << " ";
+    }    
     
     return 0;
 }
