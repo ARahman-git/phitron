cd4191cbb0f077433ed428052200445cec799029
ARahman-git <139566806+ARahman-git@users.noreply.github.com>
2026-07-04 22:01:07 +0600

    ds

---
+++ Basic Data Structures/module 07/deleteHead.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int val;
+    Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+
+    };
+};
+
+void insert_at_head(Node* &head, int val)
+{
+    Node* newnode = new Node(val);
+    newnode->next = head;
+    head = newnode;      
+}
+
+void insert_at_tail_optimized(Node* &head, int val, Node* &tail)
+{
+    Node* newnode = new Node(val);
+
+    if(head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    
+    // Node* temp = head;
+    // while(temp->next != NULL)
+    // {
+    //     temp = temp->next;
+    // }
+    // temp->next = newnode;
+    
+    tail->next = newnode;
+    tail = tail->next;
+
+}
+
+void delete_at_head(Node* &head)
+{
+    Node* deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+
+void print_lnkdList(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void print_reverse(Node* temp)
+{
+    if(temp == NULL)
+        return;
+    print_reverse(temp->next);
+    cout << temp->val << endl;
+    
+}
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(1)
+    {
+        int val;
+        cin >> val;
+        if(val == -1)
+            break;
+        insert_at_tail_optimized(head, val, tail);
+    }
+
+    delete_at_head(head);
+    print_lnkdList(head);
+    return 0;
+}
+
